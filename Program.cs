694e0726effde88a86729688c0bbd5711d12fd13
enepu <enepu@LAPTOP-SO1FSLGE>
2021-12-03 00:55:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace p17_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int kol = 0;
+                while (true)
+                {
+                    Console.Write("количество элементов массива: ");
+                    kol = int.Parse(Console.ReadLine());
+                    if (kol > 0) { break; }
+                }
+                double[] arr = new double[kol];
+                for (int i = 0; i < kol; i++)
+                {
+                    Console.Write($"{i+1} элемент массива: ");
+                    arr[i] = Convert.ToDouble(Console.ReadLine());
+                }
+                Console.WriteLine("\tЗадание А");
+                foreach(var item in arr.GroupBy(u => u).Where(u => u.Count() > 0).Select(u => new { el = u.Key, ch = u.Count() }))
+                    Console.WriteLine($"{item.el} - {item.ch}");
+                Console.WriteLine("\tЗадание B");
+                foreach (var item in arr.GroupBy(u => u).Where(u => u.Count() > 0).Select(u => new { el = u.Key, ch = u.Count() }))
+                    Console.WriteLine($"{item.el * item.ch} - {item.ch}");
+            }
+            catch { Console.WriteLine("неверные данные"); }
+        }
+    }
+}
